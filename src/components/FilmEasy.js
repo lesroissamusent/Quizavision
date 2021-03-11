49b57fd57fbfe2c807b06910c593408701e41077
Hannah Tinacci <hannahtinacci@gmail.com>
2021-03-11 19:33:29 +0000

    working button functions

--- src/components/FilmEasy.js
+++ src/components/FilmEasy.js
@@ -1,41 +1,54 @@
 /*eslint-disable camelcase */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-// , { useState }
+
 const FilmEasy = ({ question, correct_answer, incorrect_answers }) => {
-  // const [userAnswer, setUserAnswer] = useState(null)
+  const [userAnswer, setUserAnswer] = useState('')
 
+ 
   const handleClick = event => {
-  //   userAnswer = event.target.value
-    console.log(event.target.value)
-    
-    if (event.target.value === 'correct') {
-      return <button className='correct'></button>
-    }
-
+    const userAnswer = event.target.value
+    setUserAnswer(userAnswer)
   }
 
-  // }
+  useEffect(() => {
+    const checkAnswer = () => {
+      if (userAnswer === correct_answer) {
+        console.log('RIGHT!') 
+      } else if ((userAnswer === incorrect_answers[0]) || (userAnswer === incorrect_answers[1]) || (userAnswer === incorrect_answers[2])) {
+        console.log('WRONG')
+      }
+    }
+    checkAnswer()
+  }, [userAnswer])
+    
 
-  // const handleIncorrect = event => {
-  //   console.log(event.target.value)
-  //  className="correct"}
+
 
   return (
     <>
       <h2>Question: {question}</h2>
 
-      <button onClick={handleClick} value="correct"> { correct_answer }</button>
-      <button onClick={handleClick} value="incorrect"> { incorrect_answers[0] }</button>
-      <button onClick={handleClick} value="incorrect"> { incorrect_answers[1] }</button>
-      <button onClick={handleClick} value="incorrect"> { incorrect_answers[2] }</button>
+      <button value={correct_answer} onClick={handleClick} className={userAnswer ? 'correct' : ''}> { correct_answer }</button>
+      <button value={incorrect_answers[0]} onClick={handleClick} > { incorrect_answers[0] }</button>
+      <button value={incorrect_answers[1]} onClick={handleClick} > { incorrect_answers[1] }</button>
+      <button value={incorrect_answers[2]} onClick={handleClick} > { incorrect_answers[2] }</button>
     </>
   )
 }
 
 export default FilmEasy
 
+
+// const array = ['hey', 'hello', 'bye']
+
+// array.map((item, i) => {
+//   return {
+//     answer: item,
+//     isCorrect: i === array.length - 1 ? true : false,
+//   }
+// })
 
 
 

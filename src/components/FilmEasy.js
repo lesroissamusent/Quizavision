af39f311bd88691873774bf251898fe778ec6cc3
Aida Bourdis <aida.lucie@gmail.com>
2021-03-11 16:16:49 +0000

    questions and answers added

--- src/components/FilmEasy.js
+++ src/components/FilmEasy.js
@@ -1,16 +1,53 @@
+/*eslint-disable camelcase */
+
 import React from 'react'
 
-
+// , { useState }
 const FilmEasy = ({ question, correct_answer, incorrect_answers }) => {
+  // const [userAnswer, setUserAnswer] = useState(null)
 
-  
+  const handleClick = event => {
+  //   userAnswer = event.target.value
+    console.log(event.target.value)
+    
+    if (event.target.value === 'correct') {
+      return <button className='correct'></button>
+    }
+
+  }
+
+  // }
+
+  // const handleIncorrect = event => {
+  //   console.log(event.target.value)
+  //  className="correct"}
+
   return (
     <>
       <h2>Question: {question}</h2>
-      <h2>Answer: { correct_answer }</h2>
-      <p>Wrong: { incorrect_answers }</p>
+
+      <button onClick={handleClick} value="correct"> { correct_answer }</button>
+      <button onClick={handleClick} value="incorrect"> { incorrect_answers[0] }</button>
+      <button onClick={handleClick} value="incorrect"> { incorrect_answers[1] }</button>
+      <button onClick={handleClick} value="incorrect"> { incorrect_answers[2] }</button>
     </>
   )
 }
 
 export default FilmEasy
+
+
+
+
+
+// const answers = [
+    
+// ]
+// const choices = answers[Math.floor(Math.random() * (answers.length))]
+
+// console.log(choices)
+
+// const answers = [correct_answer, incorrect_answers[0], incorrect_answers[1], incorrect_answers[2]]
+// const choices = answers[Math.floor(Math.random() * (answers.length))]
+
+// console.log(choices)
